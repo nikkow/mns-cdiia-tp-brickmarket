9eac387144905e6df971fca1491a3525845be1cf
Nicolas Nunge <nicolas@nicolasnunge.net>
2026-08-19 08:40:52 +0200

    feat: add products list

--- brickmarket/urls.py
+++ brickmarket/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home_route"),
     path('products/', product_list, name='product_catalogue'),
-    path('products/<slug:slug>/', product)
+    path('products/<slug:slug>/', product, name="product_details")
 ]
 
--- catalog/views.py
+++ catalog/views.py
@@ -3,14 +3,21 @@ from django.http import HttpResponse
 from .models import Product, Category
 
 def product_list(request):
+  category_slug = request.GET.get("category", "").strip()
   products = Product.objects.all()
   categories = Category.objects.all()
+
+  current_category = None
+  if category_slug:
+    current_category = get_object_or_404(Category, slug=category_slug)
+    products = products.filter(category=current_category)
 
   context = {
     "products": products,
     "categories": categories,
+    "current_category": current_category
   }
-  
+
   return render(request, "catalog/product_list.html", context)
 
 def product(request, slug:str): 
