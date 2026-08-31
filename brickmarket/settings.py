4f67c1b36c241918bce5549d7713efea06d6285d
Nicolas Nunge <nicolas@nicolasnunge.net>
2026-08-31 16:47:36 +0200

    fix: add config

--- brickmarket/settings.py
+++ brickmarket/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -138,6 +138,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
   BASE_DIR / "static"
